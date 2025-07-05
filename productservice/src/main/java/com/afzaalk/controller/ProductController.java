21e7f0a28eac541caef113002ecd679caf81e980
afzaalkhan <afzaalkhan@localhost>
2025-07-05 08:41:07 +0300

    Initial Commit

---
+++ productservice/src/main/java/com/afzaalk/controller/ProductController.java
@@ -0,0 +1,17 @@
+package com.afzaalk.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+	
+	@GetMapping("/{id}")
+	public String getProduct(@PathVariable String id) {
+		return "Product fetched with id: " + id;
+	} 
+
+}
